3e73dcc253216d924f5ed99319726f836eb12437
R_Anmol <anmolmain3061@gmail.com>
2025-12-06 23:47:00 +0530

    Exception Handling in smtplib.SMTP

--- main.py
+++ main.py
@@ -35,11 +35,18 @@ html_content = """\
 
 message.attach(MIMEText((text), "plain"))
 message.attach(MIMEText(html_content, "html"))
-
-with smtplib.SMTP(host,port) as server:
-    server.starttls()
-    server.login(login,password)
-    server.sendmail(sender_email,receiver_email,message.as_string())
-
+try:
+    with smtplib.SMTP(host,port) as server:
+        server.starttls()
+        server.login(login,password)
+        server.sendmail(sender_email,receiver_email,message.as_string())
+except smtplib.SMTPAuthenticationError:
+    print("Most probably the server didn’t accept the username/password combination provided.")   
+except smtplib.SMTPRecipientsRefused:
+    print(f"Recipient Refused: {smtplib.SMTP.sendmail()} ")
+except smtplib.SMTPResponseException as e:
+    print(f"{e.smtp_code}: {e.smtp_error}")
+except Exception:
+    print("Something wrong! Plz try again later")
 
 print("Done")
