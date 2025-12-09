bc374f8f3d8c941b47ec91c0faa0cafd29317d22
R_Anmol <anmolmain3061@gmail.com>
2025-12-09 21:04:23 +0530

    Shifted MIMEObjects inside function

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
-import smtplib
-import os
+import smtplib, sys, os
 
 from dotenv import load_dotenv
 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
+from pathlib import Path
 
 load_dotenv()
 
 def read_file(file_path):
     try:
-        with open(f"{file_path}", 'rb') as f:
+        with open(file_path, 'rb') as f:
             file = f.read()
             return file
-    except Exception as e:
-        print(f"Error {e}")
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+        print("Program Exiting --x--")
+        sys.exit()
+
+def email(plain_text, html_text, attachment_file, sender_email, receiver_email):
+    
+    mail = MIMEMultipart('mixed')
+    mail["From"] = sender_email
+    mail["To"] = receiver_email
+    mail["Subject"] = "Subject of the Email"
+
+    body = MIMEMultipart('alternative')
+    email_body_plain = plain_text
+    email_body_html = html_text
+
+    body.attach(MIMEText(email_body_plain, "plain"))
+    body.attach(MIMEText(email_body_html, "html"))
+
+    attachment = MIMEApplication(read_file(attachment_file), _subtype='pdf')
+    attachment.add_header('Content-Disposition','attachment',filename = attachment_file.name)
+    attachment.add_header('Content-Transfer-Encoding', 'base64')
+
+    mail.attach(body)
+    mail.attach(attachment)
+
+    return mail
 
 host = os.getenv('host_name')
 port = 587
@@ -26,39 +51,30 @@ password = os.getenv('password')
 sender_email = os.getenv('sender_email')
 receiver_email = os.getenv('receiver_email')
 
-mail = MIMEMultipart('mixed')
-mail["From"] = sender_email
-mail["To"] = receiver_email
-mail["Subject"] = "Subject of the Email"
+file_path = "C.pdf"
+p = Path(file_path)
 
-body = MIMEMultipart('alternative')
-email_body_plain = "Write something inside"
-email_body_html = """\
-<html>
-    <body>
-        <h1>Heading1</h1>
-        <p>Lorem Ipsum</p>
-        <h2>Heading2</h2>
-        <p>Lorem Ipsum Lorem Ipsum</p>
-    </body>
-<html>
-    """
 
-body.attach(MIMEText(email_body_plain, "plain"))
-body.attach(MIMEText(email_body_html, "html"))
 
-attachment = MIMEApplication(read_file("C.pdf"), _subtype='pdf')
-attachment.add_header('Content-Disposition','attachment',filename = "C.pdf")
-attachment.add_header('Content-Transfer-Encoding', 'base64')
+text = "Write something inside"
+html = """\
+    <html>
+        <body>
+            <h1>Heading1</h1>
+            <p>Lorem Ipsum</p>
+            <h2>Heading2</h2>
+            <p>Lorem Ipsum Lorem Ipsum</p>
+        </body>
+    </html>
+        """
 
-mail.attach(body)
-mail.attach(attachment)
+
 
 try:
     with smtplib.SMTP(host,port) as server:
         server.starttls()
         server.login(login,password)
-        server.sendmail(sender_email,receiver_email,mail.as_string())
+        server.sendmail(sender_email,receiver_email,email(text,html,p,sender_email,receiver_email).as_string())
 
 except smtplib.SMTPAuthenticationError:
     print("Most probably the server didn’t accept the username/password combination provided.")   
@@ -71,3 +87,6 @@ except Exception:
 
 else:
     print("Email was sent successfully")
+
+
+
