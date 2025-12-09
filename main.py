4e45c8aa39be10da886016082d0f1394ec6f8e2f
R_Anmol <anmolmain3061@gmail.com>
2025-12-09 16:20:30 +0530

    Email Attachment Feature

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ from dotenv import load_dotenv
 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 load_dotenv()
+
+def read_file(file_path):
+    try:
+        with open(f"{file_path}", 'rb') as f:
+            file = f.read()
+            return file
+    except Exception as e:
+        print(f"Error {e}")
 
 host = os.getenv('host_name')
 port = 587
@@ -17,14 +26,13 @@ password = os.getenv('password')
 sender_email = os.getenv('sender_email')
 receiver_email = os.getenv('receiver_email')
 
+mail = MIMEMultipart('mixed')
+mail["From"] = sender_email
+mail["To"] = receiver_email
+mail["Subject"] = "Subject of the Email"
+
+body = MIMEMultipart('alternative')
 email_body_plain = "Write something inside"
-
-message = MIMEMultipart('alternative')
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = "Subject of the Email"
-
-
 email_body_html = """\
 <html>
     <body>
@@ -36,15 +44,21 @@ email_body_html = """\
 <html>
     """
 
-message.attach(MIMEText(email_body_plain, "plain"))
-message.attach(MIMEText(email_body_html, "html"))
+body.attach(MIMEText(email_body_plain, "plain"))
+body.attach(MIMEText(email_body_html, "html"))
 
+attachment = MIMEApplication(read_file("C.pdf"), _subtype='pdf')
+attachment.add_header('Content-Disposition','attachment',filename = "C.pdf")
+attachment.add_header('Content-Transfer-Encoding', 'base64')
+
+mail.attach(body)
+mail.attach(attachment)
 
 try:
     with smtplib.SMTP(host,port) as server:
         server.starttls()
         server.login(login,password)
-        server.sendmail(sender_email,receiver_email,message.as_string())
+        server.sendmail(sender_email,receiver_email,mail.as_string())
 
 except smtplib.SMTPAuthenticationError:
     print("Most probably the server didn’t accept the username/password combination provided.")   
